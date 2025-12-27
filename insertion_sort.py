86e566e0d1ec4efb3ff06bae8218fe027b523316
MauroRaya <maurorayabr@gmail.com>
2025-12-26 22:29:10 -0300

    refactor and feat: remove unecessary steps and add basic unit testing

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,10 +1,12 @@
 def insertion_sort(list: list[int]):
-    length: int = len(list)
+    length = len(list)
 
     for i in range(1, length):
-        temp: int = list[i]
+        temp = list[i]
+        j = i - 1
 
-        for j in range(i - 1, -1, -1):
-            if list[j] > temp:
-                list[j + 1] = list[j]
-                list[j] = temp
+        while j >= 0 and list[j] > temp:
+            list[j + 1] = list[j]
+            j -= 1
+        
+        list[j + 1] = temp
---
+++ tests/test_insertion_sort.py
@@ -0,0 +1,38 @@
+import unittest
+from insertion_sort import insertion_sort
+
+
+class TestInsertionSort(unittest.TestCase):
+    def test_empty_list(self):
+        list = []
+        insertion_sort(list)
+        self.assertEqual(list, [])
+
+    def test_single_element(self):
+        list = [5]
+        insertion_sort(list)
+        self.assertEqual(list, [5])
+
+    def test_sorted_list(self):
+        list = [1, 2, 3, 4, 5]
+        insertion_sort(list)
+        self.assertEqual(list, [1, 2, 3, 4, 5])
+
+    def test_reverse_sorted_list(self):
+        list = [5, 4, 3, 2, 1]
+        insertion_sort(list)
+        self.assertEqual(list, [1, 2, 3, 4, 5])
+
+    def test_unsorted_list(self):
+        list = [3, 1, 4, 5, 2]
+        insertion_sort(list)
+        self.assertEqual(list, [1, 2, 3, 4, 5])
+
+    def test_edge_case_insertion(self):
+        list = [10, 2, 3, 4, 5]
+        insertion_sort(list)
+        self.assertEqual(list, [2, 3, 4, 5, 10])
+
+
+if __name__ == '__main__':
+    unittest.main()
